96581c87810b5d8f9489128cb65c1282c2e69cca
abhishek darwai <123742492+abhi101995@users.noreply.github.com>
2023-08-25 16:15:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+    makesound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+//Detecting ktyboard press
+   document.addEventListener("keypress",function(event){
+
+        makesound(event,key);
+        buttonAnimation(eventkey);
+   });
+      function makesound(key){
+       switch (key) {
+        case "w":
+      var tom1 = new Audio("./tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("./tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('./tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('./tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('./snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('./crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('.//kick-bass.mp3');
+      kick.play();
+      break;
+
+
+    default: console.log(buttonInnerHTML);
+
+  }
+}
+   
+
+   function buttonAnimation(currentkay){
+    var activebutton=document.querySelector(""+currentkay);
+      activebutton.classList.add("pressed");
+}
